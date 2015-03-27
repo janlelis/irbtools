fcc2e1caba545aa6231563006f4424c44e0ef862
Jan Lelis <mail@janlelis.de>
2015-03-27 18:10:48 +0100

    don't load binding_of_caller

--- lib/irbtools/more.rb
+++ lib/irbtools/more.rb
@@ -15,10 +15,6 @@ Irbtools.add_library 'looksee', late_thread: :c do
   class Object; def lp() l end; end
 end
 
-# repl method
-Irbtools.add_library 'binding_of_caller', thread: 'more_3'
-Irbtools.add_library 'debugging/repl', thread: 'more_3'
-
 # new guessmethod
 Irbtools.add_library 'did_you_mean', thread: 'more_4'
 
