7f5fcdddf81aa9e7a8ebd488b693929baf5f8413
Jan Lelis <mail@janlelis.de>
2015-03-28 17:42:45 +0100

    tweak irbtools-more

--- lib/irbtools/more.rb
+++ lib/irbtools/more.rb
@@ -5,18 +5,17 @@ require_relative 'more/version'
 require 'irbtools/configure'
 
 # Better auto-completion
-Irbtools.add_library :bond, :thread => 'more_2' do
+Irbtools.add_library :bond, thread: :more1 do
   Bond.start :gems => %w[irbtools]
 end
 
 # Object#l method for inspecting its lookup path
-Irbtools.add_library 'looksee', late_thread: :c do
-  Looksee.rename :l
-  class Object; def lp() l end; end
+Irbtools.add_library 'looksee', thread: :more2 do
+  Looksee.rename :lp
 end
 
 # new guessmethod
-Irbtools.add_library 'did_you_mean', thread: 'more_4'
+Irbtools.add_library 'did_you_mean', thread: :more3
 
 # load now
 Irbtools.start if standalone
