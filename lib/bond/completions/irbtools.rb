e9a7c5894d80a32fb647c431c8f32858e7173f07
Jan Lelis <mail@janlelis.de>
2015-03-28 17:38:37 +0100

    add a fewe auto-completions

--- lib/bond/completions/irbtools.rb
+++ lib/bond/completions/irbtools.rb
@@ -1,17 +1,49 @@
-# encoding: utf-8
+# # #
+# completion actions
 
-# TODO more ;)
+none = ->(*){ [] }
 
-def send_methods(obj)
-  (obj.methods + obj.private_methods(false)).map {|e| e.to_s } - Mission::OPERATORS
-end
-
-complete(:method=>"Object#ri") {|e|
+send_completion = ->(e){
  if e.arguments.empty?
    [':']
  else
-   # from bond send
-   (e.object.methods + e.object.private_methods(false)).map {|e| e.to_s } - Mission::OPERATORS
+   e.object.methods.map(&:to_s) - Mission::OPERATORS
  end
 }
 
+public_send_completion = ->(e){
+ if e.arguments.empty?
+   [':']
+ else
+   e.object.public_methods.map(&:to_s) - Mission::OPERATORS
+ end
+}
+
+
+# # #
+# base ruby
+
+complete method: "Object#send", &send_completion
+complete method: "Object#method", &send_completion
+complete method: "Object#public_method", &public_send_completion
+complete method: "Object#public_send", &public_send_completion
+
+# # #
+# irbtools
+
+# methods
+complete method: "#howtocall", &send_completion
+complete method: "#code", &send_completion
+complete method: "Object#ri", &send_completion
+
+
+# nops
+complete method: "#reset!", &none
+complete method: "#clear", &none
+complete method: "#beep", &none
+complete method: "#session_history", &none
+complete method: "#info", &none
+complete method: "#version", &none
+complete method: "#engine", &none
+complete method: "#os", &none
+complete method: "Object#lp", &none
