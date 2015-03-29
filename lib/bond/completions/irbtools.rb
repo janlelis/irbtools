933c7e5e91db741f93b9a85f15f31f11715dd225
Jan Lelis <mail@janlelis.de>
2015-03-29 12:53:04 +0200

    more bond completions

--- lib/bond/completions/irbtools.rb
+++ lib/bond/completions/irbtools.rb
@@ -19,6 +19,42 @@ public_send_completion = ->(e){
  end
 }
 
+directories = ->(e){
+  if e.arguments.empty?
+    ['"']
+  else
+    files(e).select{ |f| File.directory?(f) }
+  end
+}
+
+files = ->(e){
+  if e.arguments.empty?
+    ['"']
+  else
+    files(e)
+  end
+}
+
+
+ruby_files = ->(e){
+  if e.arguments.empty?
+    ['"']
+  else
+    Dir["./**/*.{rb,bundle,dll,so}"].map{ |f| f.sub('./', '') }.uniq
+  end
+}
+
+ruby_files_with_load_path = ->(e){
+  if e.arguments.empty?
+    ['"']
+  else
+    ($:.flat_map{ |lf|
+      Dir["#{lf}/**/*.{rb,bundle,dll,so}"].map{ |f| f.sub(lf + '/', '') }
+    } + Gem.path.flat_map{ |gm|
+      Dir["#{gm}/gems/*/lib/*.{rb,bundle,dll,so}"].map{ |f| f.sub(/^.*\//,'') }
+    }).map{ |f| f.sub(/\.(?:rb|bundle|dll|so)$/, '') }.uniq
+  end
+}
 
 # # #
 # base ruby
@@ -28,6 +64,14 @@ complete method: "Object#method", &send_completion
 complete method: "Object#public_method", &public_send_completion
 complete method: "Object#public_send", &public_send_completion
 
+complete method: "#require", search: :files, &ruby_files_with_load_path
+complete method: "#rq", search: :files, &ruby_files_with_load_path
+complete method: "#rerequire", search: :files, &ruby_files_with_load_path
+complete method: "#rrq", search: :files, &ruby_files_with_load_path
+complete method: "#require_relative", &ruby_files
+complete method: "#rr", &ruby_files
+complete method: "#load", &ruby_files
+
 # # #
 # irbtools
 
@@ -36,6 +80,10 @@ complete method: "#howtocall", &send_completion
 complete method: "#code", &send_completion
 complete method: "Object#ri", &send_completion
 
+# files
+complete method: "ls", &directories
+complete method: "cd", &directories
+complete method: "cat", &files
 
 # nops
 complete method: "#reset!", &none
