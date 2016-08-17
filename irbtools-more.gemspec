0a3708f0a15c2ff321ac04f953c8cb20963ed658
Jan Lelis <mail@janlelis.de>
2016-08-17 17:59:02 +0200

    Re-enable looksee (not comptible with Ruby 2.3, but does not crash anymore during install)

--- irbtools-more.gemspec
+++ irbtools-more.gemspec
@@ -26,7 +26,7 @@ Gem::Specification.new do |s|
   s.required_ruby_version = '~> 2.0'
   s.add_dependency 'irbtools', '~> 2.0'
   s.add_dependency 'bond',     '~> 0.5'
-  # s.add_dependency 'looksee' , '~> 3.1'
+  s.add_dependency 'looksee' , '~> 4.0'
   s.add_dependency 'binding_of_caller', '~> 0.7'
   s.add_dependency 'did_you_mean', '~> 1.0'
 end
--- lib/irbtools/more.rb
+++ lib/irbtools/more.rb
@@ -9,10 +9,14 @@ Irbtools.add_library :bond, thread: :more1 do
   Bond.start :gems => %w[irbtools]
 end
 
-# Object#l method for inspecting its lookup path
-# Irbtools.add_library 'looksee', thread: :more2 do
-#   Looksee.rename :lp
-# end
+begin
+  # Object#l method for inspecting its lookup path
+  Irbtools.add_library 'looksee', thread: :more2 do
+    Looksee.rename :lp
+  end
+rescue LoadError
+  # do not load if not supported
+end
 
 # new guessmethod
 Irbtools.add_library 'did_you_mean', thread: :more3
