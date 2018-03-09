21f9857e56e1ebc42c9e7d20074f6813df04ebe6
Jan Lelis <mail@janlelis.de>
2018-03-09 20:29:56 +0100

    Do not use binding.repl gem if Ruby version is at least 2.5

--- lib/irbtools/binding.rb
+++ lib/irbtools/binding.rb
@@ -1,19 +1,29 @@
-require 'binding.repl'
-BindingRepl.auto = %w[irb ripl ir rib pry]
-
-class Binding
-  alias irb repl!
-end
-
 begin
   require 'binding_of_caller'
 rescue LoadError
 end
 
-if defined? BindingOfCaller
-  require 'debugging/repl'
+if RUBY_VERSION >= "2.5.0"
+  if defined? BindingOfCaller
+    module Kernel
+      private def irb
+        binding.of_caller(1).irb
+      end
+    end
+  end
+else
+  require 'binding.repl'
+  BindingRepl.auto = %w[irb ripl ir rib pry]
 
-  module Debugging
-    alias irb repl
+  class Binding
+    alias irb repl!
+  end
+
+  if defined? BindingOfCaller
+    require 'debugging/repl'
+
+    module Debugging
+      alias irb repl
+    end
   end
 end
