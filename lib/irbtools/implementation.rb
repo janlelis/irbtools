b5bac7e9f14d16fafe6700bbc9310dd7fd86cd77
Jan Lelis <hi@ruby.consulting>
2022-12-27 22:20:55 +0100

    Structure configure_irb! method better

--- lib/irbtools/implementation.rb
+++ lib/irbtools/implementation.rb
@@ -101,29 +101,29 @@ module Irbtools
       $VERBOSE, $DEBUG = remember_verbose_and_debug
     end
 
-    # configure irb
     def configure_irb!
       if defined?(IRB)
         IRB.conf[:AUTO_INDENT]  = true                 # simple auto indent
         IRB.conf[:EVAL_HISTORY] = 42424242424242424242 # creates the special __ variable
         IRB.conf[:SAVE_HISTORY] = 2000                 # how many lines will go to ~/.irb_history
-
-        # prompt
-        (IRB.conf[:PROMPT] ||= {} ).merge!( {:IRBTOOLS => {
-          :PROMPT_I => ">> ",    # normal
-          :PROMPT_N => "|  ",    # indenting
-          :PROMPT_C => " > ",    # continuing a statement
-          :PROMPT_S => "%l> ",   # continuing a string
-          :RETURN   => "=> %s \n",
-          :AUTO_INDENT => true,
-        }})
-
-        IRB.conf[:PROMPT_MODE] = :IRBTOOLS
-
+        set_propmt
         load_commands
         add_command_aliases
         rename_ls_to_ils
       end
+    end
+
+    def set_propmt
+      (IRB.conf[:PROMPT] ||= {} ).merge!( {:IRBTOOLS => {
+        :PROMPT_I => ">> ",    # normal
+        :PROMPT_N => "|  ",    # indenting
+        :PROMPT_C => " > ",    # continuing a statement
+        :PROMPT_S => "%l> ",   # continuing a string
+        :RETURN   => "=> %s \n",
+        :AUTO_INDENT => true,
+      }})
+
+      IRB.conf[:PROMPT_MODE] = :IRBTOOLS
     end
 
     def load_commands
@@ -141,7 +141,7 @@ module Irbtools
     end
 
     def add_command_aliases
-      IRB.conf[:COMMAND_ALIASES].merge!({
+      IRB.conf[:COMMAND_ALIASES] = (IRB.conf[:COMMAND_ALIASES] || {}).merge({
         :ri => :show_doc,
         :'$' => :sys,
         :'+' => :shadow,
